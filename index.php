d1fd66b535f3399da48a5adf82e5186a21f3dc5b
pixstudio742 <118039714+pixstudio742@users.noreply.github.com>
2022-12-17 12:31:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Picnic 2k22</title>
+    <style>
+        body{
+            
+            background-image: url('img2.jpg');
+            line-height: 0.8;
+            font-weight: 500;
+            background-repeat:no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+           
+            
+        
+            
+        }
+        font{
+            font-weight: 700;
+            font-size:40px;
+        }
+        .box{
+        width:525px;
+        height:700px;
+        background-color: rgba(240, 227, 225, 0.69);
+        padding: 35px;
+        top:50%;
+	    left:50%;
+	    position: absolute;
+        transform: translate(-50%,-37%);
+        border-radius: 7px;}
+        .form-control{
+            height: calc(1.9em + 0.75rem + 2px);
+        }
+          .mt-5, .my-5 {
+    margin-top: 1rem!important;
+}
+    </style>
+</head>
+<body>
+
+  <div class="container">  
+   <div class="box">
+    <center>
+        <font>
+            PICNIC 2K22
+        </font>
+    </center>
+    <form method="POST" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="name"><b> Name</b></label>
+        <input type="text" class="form-control" name="name" placeholder="Enter your full name" required>
+    </div>
+    <div class="form-group">
+        <label for="email"><b>Email</b></label>
+        <input type="email" class="form-control" name="email" placeholder="Enter your email" required>
+    </div>
+    <div class="form-group">
+        <label for="phone"><b>Phone</b></label>
+        <input type="number" class="form-control" name="phone" placeholder="Enter your num" required>
+    </div>
+    <div class="form-group">
+        <label for="semester"><b>Academic Year</b></label>
+        <select name="semester" class="form-control" required>
+            <option >1st Year</option>
+            <option> 2nd Year</option>
+            <option> 3rd Year</option>
+            <option> Ex Senior Student</option>
+            <option> Teachers</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label><b>Uplaod Your CV</b></label> 
+        <input class="form-control" type="file" name="image" required>  
+    </div>
+    <div class="form-group">
+        <label for="url"><b>Github Account</b></label>
+        <input type="url" class="form-control" name="url" placeholder="Enter your github acc" required>
+    </div>
+    <div class="form-group">
+        <p><b>Are you Confirm:</b></p>
+        <input type="radio" id="Yes" name="confirm" value="Yes">
+        <label for="Yes"><b>Yes</b></label>
+        <input type="radio" id="No" name="confirm" value="No">
+        <label for="No"><b>No</b></label>
+    </div>
+    <div class="form-group">
+        <div class="submit mt-5">
+            <button class="btn btn-success w-100" href="#" name="submit">
+                <span>Submit</span>
+            </button>
+        </div>
+    </div>
+
+
+
+
+    </form>
+   </div> 
+</div>
+</body>
+</html>
+<?php
+ include "database.php";
+?>
+
+<?php
+if(isset($_POST['submit'])){
+    $name = ($_POST['name']);
+    $email = ($_POST['email']);
+    $phone = ($_POST['phone']);
+    $semester = ($_POST['semester']);
+
+    $image= $_FILES["image"]["name"];
+
+    $tempname= $_FILES["image"]["tmp_name"];
+    $folder = "images/".$image;
+    $image_move = move_uploaded_file($tempname, $folder);
+
+    $url = ($_POST['url']);
+    $confirm = ($_POST['confirm']);
+
+$query =mysqli_query($conn,"insert into form(name,email,phone,semester,image,url,confirm) values('$name',' $email','$phone','$semester','$image','$url','$confirm')");
+
+
+if ( $query) {
+
+    ?>
+       <script>
+          alert("Successfully Register");
+       </script>
+     <?php 
+  
+    
+  
+    }
+    else{
+  
+      ?>
+      <script>
+         alert("Unsuccessful");
+      </script>
+    <?php 
+  
+    }
+  
+  
+    
+  
+   }
+
+?>
